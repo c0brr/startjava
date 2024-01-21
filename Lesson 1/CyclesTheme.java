3ef50f6b4b873ec587ba6197ee40493c6988e71d
c0brr <ilyaerulaev@gmail.com>
2024-01-22 01:39:31 +0400

    HW_4_change4

--- Lesson 1/CyclesTheme.java
+++ Lesson 1/CyclesTheme.java
@@ -5,77 +5,77 @@ public class CyclesTheme {
         int sumOdd = 0;
         int startSegment = -10;
         int endSegment = 21;
-        int firstTaskCounter = startSegment;
+        int segmentCounter = startSegment;
         do {
-            if (firstTaskCounter % 2 == 0) {
-                sumEven += firstTaskCounter;
+            if (segmentCounter % 2 == 0) {
+                sumEven += segmentCounter;
             } else {
-                sumOdd += firstTaskCounter;
+                sumOdd += segmentCounter;
             }
-            firstTaskCounter++;
-        } while (firstTaskCounter <= endSegment);
+            segmentCounter++;
+        } while (segmentCounter <= endSegment);
         System.out.println("В отрезке [" + startSegment + ", " + endSegment + "] сумма четных чисел = " + 
                 sumEven + ", а нечетных = " + sumOdd);
 
         System.out.println("\n2. Вывод чисел в порядке убывания");
-        int secondTaskNum1 = 10;
-        int secondTaskNum2 = 5;
-        int secondTaskNum3 = -1;
-        int max = secondTaskNum1;
-        int min = secondTaskNum1;
-        if (secondTaskNum2 >= secondTaskNum1 && secondTaskNum2 >= secondTaskNum3) {
-            max = secondTaskNum2;
-        } else if (secondTaskNum3 >= secondTaskNum1 && secondTaskNum3 >= secondTaskNum2) {
-            max = secondTaskNum3;
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = -1;
+        int max = num1;
+        int min = num1;
+        if (num2 >= num1 && num2 >= num3) {
+            max = num2;
+        } else if (num3 >= num1 && num3 >= num2) {
+            max = num3;
         }
-        if (secondTaskNum2 <= secondTaskNum1 && secondTaskNum2 <= secondTaskNum3) {
-            min = secondTaskNum2;
-        } else if (secondTaskNum3 <= secondTaskNum1 && secondTaskNum3 <= secondTaskNum2) {
-            min = secondTaskNum3;
+        if (num2 <= num1 && num2 <= num3) {
+            min = num2;
+        } else if (num3 <= num1 && num3 <= num2) {
+            min = num3;
         }
         for (int i = max - 1; i > min; i--) {
             System.out.print(i + " ");
         }
 
         System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
-        int thirdTaskNum = 1_234;
+        int num1_234 = 1_234;
         int sumDigits = 0;
         System.out.println("Реверсивное число:");
-        while (thirdTaskNum != 0) {
-            int lastDigit = thirdTaskNum % 10;
+        while (num1_234 != 0) {
+            int lastDigit = num1_234 % 10;
             sumDigits += lastDigit;
             System.out.print(lastDigit);
-            thirdTaskNum /= 10;
+            num1_234 /= 10;
         }
         System.out.println("\n\nСумма его цифр:\n" + sumDigits);
 
         System.out.println("\n4. Вывод чисел в несколько строк");
-        int fourthTaskCounter = 0;
-        int fourthTaskNum = 24;
-        for (int i = 1; i < fourthTaskNum; i += 2) {
+        int countOdds = 0;
+        int halfIntervalEnd = 24;
+        for (int i = 1; i < halfIntervalEnd; i += 2) {
             if (i < 11) {
                 System.out.printf("%2d ", i);
             } else {
                 System.out.printf("%1d ", i);
             }
-            fourthTaskCounter++;
-            if (fourthTaskCounter % 5 == 0) {
+            countOdds++;
+            if (countOdds % 5 == 0) {
                 System.out.println();
             }
         }
-        for (int i = fourthTaskNum - 1; i % 10 != 9; i += 2) {
+        for (int i = halfIntervalEnd - 1; i % 10 != 9; i += 2) {
             System.out.printf("%2d ", 0);
         }
 
         System.out.println("\n\n5. Проверка количества двоек числа на четность/нечетность");
-        int fifthTaskNum = 3_242_592;
-        int copyNum = fifthTaskNum;
+        int num3_242_592 = 3_242_592;
+        int copyNum = num3_242_592;
         int countTwos = 0;
-        while (fifthTaskNum > 0) {
-            if (fifthTaskNum % 10 == 2) {
+        while (num3_242_592 > 0) {
+            if (num3_242_592 % 10 == 2) {
                 countTwos++;
             }
-            fifthTaskNum /= 10;
+            num3_242_592 /= 10;
         }
         System.out.println(countTwos % 2 == 0 ? 
                 "В " + copyNum + " четное количество двоек - " + countTwos : 
@@ -90,36 +90,36 @@ public class CyclesTheme {
         }
         System.out.println();
 
-        int secondGeometricFigureLines = 5;
-        int secondGeometricFigureSymbols = 1;
-        while (secondGeometricFigureLines >= secondGeometricFigureSymbols ) {
-            while (secondGeometricFigureSymbols <= secondGeometricFigureLines) {
+        int rightTriangleLines = 5;
+        int rightTriangleSymbols = 1;
+        while (rightTriangleLines >= rightTriangleSymbols ) {
+            while (rightTriangleSymbols <= rightTriangleLines) {
                 System.out.print('#');
-                secondGeometricFigureSymbols++;
+                rightTriangleSymbols++;
             }
-            secondGeometricFigureLines--;
-            secondGeometricFigureSymbols = 1;
+            rightTriangleLines--;
+            rightTriangleSymbols = 1;
             System.out.println();
         }
         System.out.println();
 
-        int thirdGeometricFigureLines = 1;
-        int thirdGeometricFigureSymbols = thirdGeometricFigureLines;
-        int thirdGeometricFigureCounter = thirdGeometricFigureLines;
+        int triangleLines = 1;
+        int triangleSymbols = triangleLines;
+        int triangleCounter = triangleLines;
         do {
             do {
-                thirdGeometricFigureSymbols++;
+                triangleSymbols++;
                 System.out.print('$');
-            } while (thirdGeometricFigureSymbols <= thirdGeometricFigureCounter);
-            if (thirdGeometricFigureLines < 3) {
-                thirdGeometricFigureCounter++;
+            } while (triangleSymbols <= triangleCounter);
+            if (triangleLines < 3) {
+                triangleCounter++;
             } else {
-                thirdGeometricFigureCounter--;
+                triangleCounter--;
             }
             System.out.println();
-            thirdGeometricFigureSymbols = 1;
-            thirdGeometricFigureLines++;
-        } while (thirdGeometricFigureLines <= 5);
+            triangleSymbols = 1;
+            triangleLines++;
+        } while (triangleLines <= 5);
 
         System.out.println("\n7. Отображение ASCII-символов");
         for (int i = 1; i <= 3; i++) {
@@ -143,14 +143,14 @@ public class CyclesTheme {
         }
 
         System.out.println("\n8. Проверка, является ли число палиндромом");
-        int eighthTaskNum = 1_234_321;
-        System.out.print("Число " + eighthTaskNum);
+        int num1_234_321 = 1_234_321;
+        System.out.print("Число " + num1_234_321);
         int divider = 1_000_000;
         for (int i = 1; i <= 3; i++) {
-            if (eighthTaskNum % 10 == eighthTaskNum / divider) {
-                eighthTaskNum = eighthTaskNum % divider / 10;
+            if (num1_234_321 % 10 == num1_234_321 / divider) {
+                num1_234_321 = num1_234_321 % divider / 10;
                 divider /= 100;
-                if (eighthTaskNum > 99) {
+                if (num1_234_321 > 99) {
                     continue;
                 } else {
                     System.out.print(" является палиндромом\n");
@@ -163,23 +163,23 @@ public class CyclesTheme {
 
 
         System.out.println("\n9. Проверка, является ли число счастливым");
-        int ninthTaskNum = 613_333;
-        int copyNinthTaskNum = ninthTaskNum;
-        System.out.print("Число " + ninthTaskNum);
+        int anySixDigitNumber = 613_333;
+        int copyAnySixDigitNumber = anySixDigitNumber;
+        System.out.print("Число " + anySixDigitNumber);
         int firstHalfSum = 0;
         int secondHalfSum = 0;
         for (int i = 1; i <= 6; i++) {
             if (i > 3) {
-                firstHalfSum += ninthTaskNum % 10;
+                firstHalfSum += anySixDigitNumber % 10;
             } else {
-                secondHalfSum += ninthTaskNum % 10;
+                secondHalfSum += anySixDigitNumber % 10;
             }
-            ninthTaskNum /= 10;
+            anySixDigitNumber /= 10;
         }
         System.out.print(firstHalfSum == secondHalfSum ? " является счастливым\n" : 
                 " не является счастливым\n");
-        System.out.println("Сумма цифр " + copyNinthTaskNum / 1000 + " = " + firstHalfSum + 
-                ", а сумма " + copyNinthTaskNum % 1000 + " = " + secondHalfSum);
+        System.out.println("Сумма цифр " + copyAnySixDigitNumber / 1000 + " = " + firstHalfSum + 
+                ", а сумма " + copyAnySixDigitNumber % 1000 + " = " + secondHalfSum);
 
         System.out.println("\n10. Отображение таблицы умножения Пифагора");
         System.out.printf("%13s%10s\n", "ТАБЛИЦА", "ПИФАГОРА");
