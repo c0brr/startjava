146a66a53ffac92ba72afe674c04bb6bf74cb64a
c0brr <ilyaerulaev@gmail.com>
2024-01-25 13:18:37 +0400

    HW_MyFirstGame_change

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -3,16 +3,16 @@ import java.util.Random;
 public class MyFirstGame {
     public static void main(String[] args) {
         Random random = new Random();
-        int numComputer = random.nextInt(1, 101);
-        int numHuman = 12;
-        while (numHuman != numComputer) {
-            if (numHuman > numComputer) {
-                System.out.println("Число " + numHuman + " больше того, что загадал компьютер");
-                numHuman -= 4;
+        int computerNum = random.nextInt(1, 101);
+        int myNum = 12;
+        while (myNum != computerNum) {
+            if (myNum > computerNum) {
+                System.out.println("Число " + myNum + " больше того, что загадал компьютер");
+                myNum -= 4;
             }
-            if (numHuman < numComputer) {
-                System.out.println("Число " + numHuman + " меньше того, что загадал компьютер");
-                numHuman += 15;
+            if (myNum < computerNum) {
+                System.out.println("Число " + myNum + " меньше того, что загадал компьютер");
+                myNum += 15;
             }
         }
         System.out.println("Вы победили!");
