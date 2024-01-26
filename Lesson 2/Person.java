0ac598fa8e7683f0fb35a7a3ebed1d802163db04
c0brr <ilyaerulaev@gmail.com>
2024-01-26 19:46:07 +0400

    HW_Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "Male";
+    String name = "Mike";
+    double height = 1.86;
+    int weight = 79;
+    int age = 27;
+
+    void walk() {
+    System.out.println(name + " идет...");
+    }
+
+    void sit() {
+    System.out.println(name + " сидит...");
+    }
+
+    void run() {
+    System.out.println(name + " бежит...");
+    }
+
+    void talk() {
+    System.out.println(name + " говорит...");
+    }
+
+    void learnJava() {
+    System.out.println(name + " учит Java...");
+    }
+}
