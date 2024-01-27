bf580dc34b8a8d5950baa9eca776dc4e51af31e9
c0brr <ilyaerulaev@gmail.com>
2024-01-27 14:48:16 +0400

    HW_Wolf_change2

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,27 +1,71 @@
 class Wolf {
-    String gender;
-    String name;
-    String color;
-    double weight;
-    int age;
+    private String gender;
+    private String name;
+    private String color;
+    private double weight;
+    private int age;
 
-    void walk() {
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст\n");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void walk() {
         System.out.println(name + " идет...");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println(name + " сидит...");
     }
 
-    void run() {
+    public void run() {
         System.out.println(name + " бежит...");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println(name + " воет...");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println(name + " охотится...");
     }
 }
