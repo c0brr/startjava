1fdd3277f48544caf3896e099f07789055a1c749
c0brr <ilyaerulaev@gmail.com>
2024-01-28 15:27:32 +0400

    HW_Calculator

---
+++ Lesson 2/Calculator.java
@@ -0,0 +1,47 @@
+class Calculator {
+    private int num1;
+    private int num2;
+    private char operation;
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public void getResult() {
+        switch (operation) {
+            case '+': 
+                System.out.println("\nРезультат: " + (num1 + num2));
+                break;
+            case '-': 
+                System.out.println("\nРезультат: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("\nРезультат: " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println("\nРезультат: " + (num1 / num2));
+                break;
+            case '%':
+                System.out.println("\nРезультат: " + (num1 % num2));
+                break;
+            case '^':
+                int result = num1;
+                for (int i = 1; i < num2; i++) {
+                    result *= num1;
+                }
+                System.out.println("\nРезультат: " + result);
+                break;
+            default:
+                System.out.println("\nДанная операция не поддерживается");
+        }
+        System.out.println();
+    }
+}
