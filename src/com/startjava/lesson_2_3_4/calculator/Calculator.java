ff7e22fd8517aeeae11c2c657c500f6278023fb6
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-02-07 13:36:00 +0400

    HW_Calculator_change3

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -5,7 +5,17 @@ class Calculator {
     private int num2;
     private String operation;
 
-    public double calculate(String mathExpression) {
+    public void printResult(String mathExpression) {
+        double result = calculate(mathExpression);
+        if (result == Double.MIN_VALUE) {
+            System.out.println("Ошибка: знак " + operation + " не поддерживается");
+        } else {
+            System.out.print(num1 + " " + operation + " " + num2 + " = ");
+            System.out.print(result - (int) result == 0 ? (int) result + "\n" : String.format("%.3f\n", result));
+        }
+    }
+
+    private double calculate(String mathExpression) {
         String[] elements = mathExpression.split(" ");
         num1 = Integer.parseInt(elements[0]);
         num2 = Integer.parseInt(elements[2]);
@@ -25,16 +35,6 @@ class Calculator {
                 return Math.pow(num1, num2);
             default:
                 return Double.MIN_VALUE;
-        }
-    }
-
-    public void printResult(String mathExpression) {
-        double result = calculate(mathExpression);
-        if (result == Double.MIN_VALUE) {
-            System.out.println("Ошибка: знак " + operation + " не поддерживается");
-        } else {
-            System.out.print(num1 + " " + operation + " " + num2 + " = ");
-            System.out.print(result - (int) result == 0 ? (int) result + "\n" : String.format("%.3f\n", result));
         }
     }
 }
