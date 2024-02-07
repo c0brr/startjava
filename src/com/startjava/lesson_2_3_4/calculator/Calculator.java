20ceb93464096b40a5df7039d24dd6f41ce0794a
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-02-07 17:23:13 +0400

    HW_Calcualator_change4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,40 +1,78 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 class Calculator {
-    private int num1;
-    private int num2;
-    private String operation;
+    private static int num1;
+    private static int num2;
+    private static String operation;
+    private static boolean hasException;
 
-    public void printResult(String mathExpression) {
-        double result = calculate(mathExpression);
-        if (result == Double.MIN_VALUE) {
-            System.out.println("Ошибка: знак " + operation + " не поддерживается");
-        } else {
-            System.out.print(num1 + " " + operation + " " + num2 + " = ");
-            System.out.print(result - (int) result == 0 ? (int) result + "\n" : String.format("%.3f\n", result));
+    public static void printResult(String mathExpression) {
+        try {
+            double result = calculate(mathExpression);
+            if (!hasException) {
+                System.out.print(num1 + " " + operation + " " + num2 + " = ");
+                System.out.print(result - (int) result == 0 ? (int) result + "\n" : String.format("%.3f\n", result));
+            }
+        } catch (RuntimeException exception) {
+            System.out.println("Ошибка: математическая операция не поддерживается");
         }
+        hasException = false;
     }
 
-    private double calculate(String mathExpression) {
+    private static double calculate(String mathExpression) {
         String[] elements = mathExpression.split(" ");
-        num1 = Integer.parseInt(elements[0]);
-        num2 = Integer.parseInt(elements[2]);
-        operation = elements[1];
-        switch (operation) {
-            case "+":
-                return num1 + num2;
-            case "-":
-                return num1 - num2;
-            case "*":
-                return num1 * num2;
-            case "/":
-                return (double) num1 / num2;
-            case "%":
-                return num1 % num2;
-            case "^":
-                return Math.pow(num1, num2);
-            default:
-                return Double.MIN_VALUE;
+        if (elements.length != 3) {
+            try {
+                throw new RuntimeException();
+            } catch (RuntimeException exception) {
+                System.out.println("Ошибка: формат выражения неверен");
+                hasException = true;
+            }
+        } else {
+            try {
+                num1 = Integer.parseInt(elements[0]);
+                if (num1 <= 0) {
+                    try {
+                        throw new RuntimeException();
+                    } catch (RuntimeException exception) {
+                        printTermError("первый член выражения");
+                        hasException = true;
+                    }
+                }
+            } catch (RuntimeException exception) {
+                printTermError("первый член выражения");
+                hasException = true;
+            }
+
+            try {
+                num2 = Integer.parseInt(elements[2]);
+                if (num2 <= 0) {
+                    try {
+                        throw new RuntimeException();
+                    } catch (RuntimeException exception) {
+                        printTermError("второй член выражения");
+                        hasException = true;
+                    }
+                }
+            } catch (RuntimeException exception) {
+                printTermError("второй член выражения");
+                hasException = true;
+            }
         }
+
+        operation = elements[1];
+        return switch (operation) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> (double) num1 / num2;
+            case "%" -> num1 % num2;
+            case "^" -> Math.pow(num1, num2);
+            default -> throw new RuntimeException();
+        };
+    }
+
+    private static void printTermError(String term) {
+        System.out.println("Ошибка: " + term + " не яляется целым положительным числом");
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
         String answer = "yes";
 
         while (!answer.equals("no")) {
             if (answer.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                calculator.printResult(scanner.nextLine());
+                Calculator.printResult(scanner.nextLine());
             }
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
             answer = scanner.nextLine();
