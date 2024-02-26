dc1425b5d39a59920ee7025e66322e522cebb70a
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-02-26 16:21:59 +0400

    HW_Lesson5_change

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,17 +3,16 @@
 DROP TABLE IF EXISTS jaegers;
 
 CREATE TABLE jaegers (
-id         SERIAL PRIMARY KEY,
-model_name TEXT,
-mark       CHAR(6),
-height     REAL,
-weight     INTEGER,
-status     TEXT,
-origin     TEXT,
-launch     DATE,
-kaiju_kill INTEGER
+    id         SERIAL PRIMARY KEY,
+    model_name TEXT,
+    mark       CHAR(6),
+    height     REAL,
+    weight     INTEGER,
+    status     TEXT,
+    origin     TEXT,
+    launch     DATE,
+    kaiju_kill INTEGER
 );
 
 \include_relative init_db.sql
-
 \include_relative queries.sql
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,56 +1,61 @@
 \echo Вывод всей таблицы
 SELECT *
-FROM jaegers
-ORDER BY model_name;
+  FROM jaegers
+ ORDER BY model_name;
 
 \echo Не уничтоженные роботы
 SELECT *
-FROM jaegers
-WHERE status NOT IN('Destroyed')
-ORDER BY model_name;
+  FROM jaegers
+ WHERE status != 'Destroyed'
+ ORDER BY model_name;
 
 \echo Роботы Mark-1 и Mark-4
 SELECT *
-FROM jaegers
-WHERE mark IN('Mark-1', 'Mark-4')
-ORDER BY model_name;
+  FROM jaegers
+ WHERE mark IN('Mark-1', 'Mark-4')
+ ORDER BY model_name;
 
 \echo Все роботы кроме Mark-1 и Mark-4
 SELECT *
-FROM jaegers
-WHERE mark NOT IN('Mark-1', 'Mark-4')
-ORDER BY mark DESC;
+  FROM jaegers
+ WHERE mark NOT IN('Mark-1', 'Mark-4')
+ ORDER BY mark DESC;
 
 \echo Самый старый робот
 SELECT *
-FROM jaegers
-WHERE launch = (
-SELECT MIN(launch)
-FROM jaegers);
+  FROM jaegers
+ WHERE launch =
+       (SELECT MIN(launch)
+          FROM jaegers);
 
 \echo Роботы, которые уничтожили больше всех kaiju
-SELECT *
-FROM jaegers
-WHERE kaiju_kill = (
-SELECT MAX(kaiju_kill)
-FROM jaegers)
-ORDER BY kaiju_kill DESC;
+SELECT model_name, mark, launch, kaiju_kill
+  FROM jaegers
+ WHERE kaiju_kill =
+       (SELECT MAX(kaiju_kill)
+          FROM jaegers)
+ ORDER BY model_name;
 
 \echo Средний вес роботов
-SELECT round(AVG(weight), 3)
-FROM jaegers;
+SELECT ROUND(AVG(weight), 3) AS average_weight
+  FROM jaegers;
 
-\echo +1 kaiju у роботов, которые не разрушены
+\echo +1 убийство kaiju у роботов, которые не разрушены
 UPDATE jaegers
-SET kaiju_kill = kaiju_kill + 1
-WHERE status NOT IN('Destroyed');
+   SET kaiju_kill = kaiju_kill + 1
+ WHERE status != 'Destroyed';
+
+\echo Вывод таблицы
 SELECT *
-FROM jaegers
-ORDER BY model_name;
+  FROM jaegers
+ ORDER BY model_name;
 
 \echo Удаление уничтоженных роботов
-DELETE FROM jaegers
-WHERE status IN('Destroyed');
+DELETE
+  FROM jaegers
+ WHERE status = 'Destroyed';
+
+\echo Вывод таблицы
 SELECT *
-FROM jaegers
-ORDER BY model_name;
+  FROM jaegers
+ ORDER BY model_name;
